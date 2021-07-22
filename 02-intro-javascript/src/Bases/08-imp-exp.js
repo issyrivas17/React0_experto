2e0b6a94b897d1ce9dff447870d80cd4fdebfbac
issyrivas17 <ismeniarivas17@gmail.com>
2021-07-22 19:07:50 -0400

    Importaciones y exportaciones

--- 02-intro-javascript/src/Bases/08-imp-exp.js
+++ 02-intro-javascript/src/Bases/08-imp-exp.js
@@ -6,7 +6,7 @@ import { heroes,owners } from "./Bases/data/heroes";
 const getHeroebyId = (id) => heroes.find ((heroe)=> heroe.id === id );
 
 console.log(getHeroebyId(2));  
-console.log (owners)
+console.log (owners); 
 //find?,filter 
 const getHeroesByOwner = (owner) => heroes.filter((heroe)=> heroe.owner === owner );
 
