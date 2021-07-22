e88f2d4c0188eb929b2ff37326848ad7cf25b580
issyrivas17 <ismeniarivas17@gmail.com>
2021-07-22 15:46:20 -0400

    Arreglos y operador spread

---
+++ 02-intro-javascript/src/Bases/04-arreglos.js
@@ -0,0 +1,27 @@
+ //Arreglos en JS  
+        
+       // const arreglo = new Array (120);
+
+       const arreglo= [1,2,3,4]; 
+       /*  arreglo.push(1)
+        arreglo.push(2)
+        arreglo.push(3)
+        arreglo.push(4) */ 
+
+        let arreglo2 = [...arreglo,5];
+        //arreglo2.push (5); 
+
+        const arreglo3= arreglo2.map(function(numero){
+            return numero * 2; 
+
+       });
+        console.log (arreglo); 
+        console.log(arreglo2); 
+        console.log(arreglo3); 
+
+           
+
+
+        //... operador spread -> manda indivualmente los elementos- 
+
+        
